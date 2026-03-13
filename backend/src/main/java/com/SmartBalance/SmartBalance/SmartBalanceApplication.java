f9ffc3f7910785ea768365b69f3044e71ed87116
Gabriel Pereira <gabrielpereira160101@gmail.com>
2026-03-13 20:15:10 -0300

    start project

---
+++ backend/src/main/java/com/SmartBalance/SmartBalance/SmartBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.SmartBalance.SmartBalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBalanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/SmartBalance/SmartBalance/SmartBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartBalance.SmartBalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
